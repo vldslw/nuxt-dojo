130b1f96fd7354ed4307dddf517b25ea94fe23df
Vladislav Pavlov <vldslw@gmail.com>
2023-12-26 20:06:00 -0300

    add frontend part

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore aperiam sapiente eos, facilis amet dolore enim nesciunt fugit cum possimus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas earum sint repudiandae impedit cumque reiciendis consectetur commodi voluptatibus asperiores labore.</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+
+  p {
+    margin: 20px 0;
+  }
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+         <ProductCard :product="p"/>
+        <!-- <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  definePageMeta({
+    layout: 'products'
+  })
+
+  const { data: products } = await useFetch('https://fakestoreapi.com/products') 
+
+  useHead({
+    title: 'Nuxt Dojo | Merch',
+    meta: [{
+      name: 'description', content: 'Nuxt 3 Merch'
+    }]
+  })
+
+</script>
+
+<style scoped>
+
+</style>
